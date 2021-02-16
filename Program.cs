d715fa7f41b120f4f8e3091620052764812de420
Vitor Vieira <vitorlucena@outlook.com.br>
2021-02-16 01:42:48 -0300

    intro

---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace revisao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            aluno[] alunos = new aluno[5];
+            int ialunos = 0;
+            string userOption = getUserOption();
+
+            while (userOption.ToUpper() != "X")
+            {
+                switch (userOption)
+                {
+                    case "1":
+                        Console.WriteLine("Informe o nome do aluno: ");
+                        aluno aluno = new aluno();
+                        aluno.nome = Console.ReadLine();
+
+                        Console.WriteLine("Informe a nota do aluno: ");
+                        if(decimal.TryParse(Console.ReadLine(), out decimal nota)){
+                            aluno.nota = nota;
+                        }
+                        else{
+                            throw new ArgumentException("valor da nota deve ser decimal");
+                        }     
+                        alunos[ialunos] = aluno;
+                        ialunos++;        
+
+                        break;
+                    case "2":
+                        foreach(var a in alunos){
+                          if (a.nome != null){  
+                            Console.WriteLine($"Aluno: {a.nome} - Nota: {a.nota} ");
+                          }
+                        }
+
+                        break;
+                    case "3":
+
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+
+                }
+
+                userOption = getUserOption();
+            }
+        }
+
+        private static string getUserOption()
+        {
+            Console.WriteLine("Informe o que tu quer: ");
+            Console.WriteLine("1- Inserir aluno: ");
+            Console.WriteLine("2- Listar Alunos:  ");
+            Console.WriteLine("3- Calcular media da turma: ");
+            Console.WriteLine("X- Sair ");
+            Console.WriteLine();
+
+            string userOption = Console.ReadLine();
+
+            Console.WriteLine();
+            return userOption;
+        }
+    }
+}
---
+++ aluno.cs
@@ -0,0 +1,9 @@
+namespace revisao
+{
+    public struct aluno
+    {
+        public string nome { get; set;}
+
+        public decimal nota { get; set; }
+    }
+}
